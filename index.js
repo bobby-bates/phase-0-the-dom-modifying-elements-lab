ea471202ae656f2b84c7ee17f81269876bb77628
Bobby Bates <robertfbatesjr@gmail.com>
2022-04-24 19:21:42 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// Write your code here!
+// Remove main HTML tag:
+document.getElementById('main').remove()
+
+// Add new element with ID of 'victory':
+const newHeader = document.createElement('h1')
+newHeader.id = 'victory'
+// Use innerText instead; test requires innerHTML
+newHeader.innerHTML = 'Bobby is the champion!'
+newHeader.style.color = 'cyan'
+document.body.append(newHeader)
